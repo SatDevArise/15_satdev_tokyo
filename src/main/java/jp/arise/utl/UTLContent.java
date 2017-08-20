9c1223713489701be09a910e572fc0d3f59d0b53
SatDevArise <prj.sat.dev@arise.co.jp>
2017-08-20 20:54:01 +0900

    西沢 淳 2017年8月20日
    対応名：15期土曜勉強会_東京
    コメント：初期コミット

---
+++ src/main/java/jp/arise/utl/UTLContent.java
@@ -0,0 +1,60 @@
+package jp.arise.utl;
+
+/**
+ * COM用定数用クラス
+ * プロジェクト内で共通利用される定数
+ * @author AtsushiNishizawa
+ * @since 2017/07/17
+ */
+public class UTLContent {
+
+	/**
+	 * String型：0
+	 */
+	public String STR_ZERO = "0";
+
+	/**
+	 * String型：1
+	 */
+	public String STR_ONE = "1";
+
+	/**
+	 * String型：2
+	 */
+	public String STR_TWO = "2";
+
+	/**
+	 * String型：空白
+	 */
+	public String BLANK = " ";
+
+	/**
+	 * int型：0
+	 */
+	public int INT_ZERO = 0;
+
+	/**
+	 * int型：1
+	 */
+	public int INT_ONE = 1;
+
+	/**
+	 * String型：COMGM001
+	 * ログイン画面ID
+	 */
+	public String GMID_COMGM001 = "COMGM001";
+
+	/**
+	 * String型：COMGM002
+	 * メニュー画面ID
+	 */
+	public String GMID_COMGM002 = "COMGM002";
+
+
+
+
+
+
+
+
+}
