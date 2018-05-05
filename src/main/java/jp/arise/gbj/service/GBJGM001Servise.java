d9ae796ba004433f8e73999a63076c153ae9e12a
SatDevArise <prj.sat.dev@arise.co.jp>
2018-05-05 16:01:42 +0900

    櫻田　恵里奈 2018年5月5日                        　　 ←タイトル 　　　　　　　　　　　　　　　　　　　　　　　 対応名：15期土曜勉強会 コメント：空白文字を追加　　

--- src/main/java/jp/arise/gbj/service/GBJGM001Servise.java
+++ src/main/java/jp/arise/gbj/service/GBJGM001Servise.java
@@ -40,23 +40,23 @@ public class GBJGM001Servise {
 	 * @since 2018/1/8
 	 */
 	public List<GBJGM001Form> setGBJGM001FormList(List<COMGM003Dto> genbaList) {
-		//GBJGM001Formのインスタンスを作成
-		GBJGM001Form GBJGM001Form = new GBJGM001Form();
+
 		//GBJGM001FormのListを作成
 		List<GBJGM001Form> GBJGM001FormList = new ArrayList<GBJGM001Form>();
 
 		if (genbaList != null) {
 			for (int i = 0; i < genbaList.size(); i++) {
-
+				//GBJGM001Formのインスタンスを作成
+				GBJGM001Form GBJGM001Form = new GBJGM001Form();
 				//検索で取得した1レコードをFormに設定
 				GBJGM001Form.setUserId(genbaList.get(i).getUserId());
-//				GBJGM001Form.setGenbaId(genbaList.get(i).getGenbaId());
+				GBJGM001Form.setGenbaId(genbaList.get(i).getGenbaId());
 				GBJGM001Form.setGenbaNa(genbaList.get(i).getGenbaNa());
 				GBJGM001Form.setAddress(genbaList.get(i).getAddress());
 				GBJGM001Form.setMoyori1Station(genbaList.get(i).getMoyori1Station());
 				GBJGM001Form.setMoyori2Station(genbaList.get(i).getMoyori2Station());
 				GBJGM001Form.setMoyori3Station(genbaList.get(i).getMoyori3Station());
-//				GBJGM001Form.setWork(genbaList.get(i).getWork());
+				GBJGM001Form.setWork(genbaList.get(i).getWork());
 				GBJGM001Form.setPhase(genbaList.get(i).getPhase());
 //				GBJGM001Form.setPage(genbaList.get(i).getPage());
 				GBJGM001FormList.add(GBJGM001Form);
